1a49e2c634ad0d6fc882e240b1d2745f2859ce0f
avwo <avwu@qq.com>
2019-05-23 23:47:24 +0800

    test: init

---
+++ test/proxy.test.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const https = require('https');
+
+const { setProxy } = require('../lib');
+
+const PROXY_OPTIONS = {
+  host: '127.0.0.1',
+  port: 8888,
+};
+// 动态设置代理
+// setProxy((options) => {
+//   return Object.assign(options, PROXY_OPTIONS);
+// });
+
+// 设置固定代理 
+setProxy(PROXY_OPTIONS);
+
+const httpClient = http.request('http://ke.qq.com', (res) => {
+  console.log(res.statusCode); // eslint-disable-line
+});
+httpClient.on('error', () => {});
+httpClient.end();
+
+const httpsClient = https.request('https://ke.qq.com', (res) => {
+  console.log(res.statusCode); // eslint-disable-line
+});
+httpsClient.on('error', () => {});
+httpsClient.end();
---
+++ test/socket.test.js
@@ -0,0 +1,29 @@
+const { setProxy, createConnection } = require('../lib');
+
+const PROXY_OPTIONS = {
+  host: '127.0.0.1',
+  port: 8888,
+};
+
+// 动态设置代理
+setProxy((options) => {
+  return Object.assign(options, PROXY_OPTIONS);
+});
+
+// 设置固定代理 
+// setProxy(PROXY_OPTIONS);
+
+(async () => {
+  const socket = await createConnection({
+    host: 'ke.qq.com',
+    port: 80,
+  });
+  socket.write([
+    'GET / HTTP/1.1',
+    'Host: ke.qq.com',
+    '\r\n',
+  ].join('\r\n'));
+  socket.on('data', (data) => {
+    console.log(`${data}`); // eslint-disable-line
+  });
+})();
