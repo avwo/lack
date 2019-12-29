5d268d7ccf646ac2cbfb5152f261660c53a2542b
avwo <avwu@qq.com>
2019-12-29 16:35:09 +0800

    Release v0.2.4

--- assets/server.js
+++ assets/server.js
@@ -3,15 +3,18 @@ module.exports = (server/* , options */) => {
   // handle http request
   server.on('request', (req, res) => {
     // do something
+    req.passThrough();
   });
 
   // handle websocket request
   server.on('upgrade', (req, socket) => {
     // do something
+    req.passThrough();
   });
 
   // handle tunnel request
   server.on('connect', (req, socket) => {
     // do something
+    req.passThrough();
   });
 };
