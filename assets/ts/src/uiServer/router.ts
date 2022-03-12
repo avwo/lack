79caa147a59c00e731d776dbcc011f20ace2329a
avwo <avwu@qq.com>
2022-03-12 20:32:03 +0800

    refactor: srcc -> lib

--- assets/ts/src/uiServer/router.ts
+++ assets/ts/src/uiServer/router.ts
@@ -1,5 +1,7 @@
+import { Router } from 'koa-router';
+
 // For help see https://github.com/ZijianHe/koa-router#api-reference
-export default (router) => {
+export default (router: Router) => {
   // router.get('/cgi-bin/init', (ctx) => {
   //   ctx.body = 'Hello whistle.';
   // });
--- assets/ts/src/wsResWrite.ts
+++ assets/ts/src/wsResWrite.ts
@@ -1,5 +1,5 @@
 
-export default (server: Whistle.PluginServer/*, options: Whistle.PluginOptions*/) => {
+export default (server: Whistle.PluginServer, options: Whistle.PluginOptions) => {
   server.on('connect', (req: Whistle.PluginRequest, socket: Whistle.PluginSocket) => {
     socket.pipe(socket);
   });
