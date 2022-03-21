5bc7b53e7140d0898b3007448a34265234a6f677
avwo <avwu@qq.com>
2022-03-21 19:07:06 +0800

    chore: eslint-disable @typescript-eslint/camelcase

--- assets/ts/src/types/base.d.ts
+++ assets/ts/src/types/base.d.ts
@@ -4,6 +4,7 @@ import { IncomingMessage, ServerResponse, Server } from 'http';
 import { ParsedUrlQuery } from 'querystring';
 import { Socket } from 'net';
 
+/* eslint-disable @typescript-eslint/camelcase */
 declare global {
   namespace WhistleBase {
     class Request extends IncomingMessage {}
